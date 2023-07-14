5eeed1a7d4dab99de1c4f8aa4ba548e189595a9a
laura-maxst <jykim@maxst.com>
2023-07-14 13:23:40 +0900

    delete icons 추가

---
+++ packages/icons/src/icons/deleteLineLight.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const DeleteLineLightIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="48"
+      height="48"
+      viewBox="0 0 48 48"
+      fill="none"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M19.4599 4.62H28.5399V5.82H19.4599V4.62ZM39.1598 9.66003H13.1998L8.83978 14.02V17.2H35.0798V38.56L31.4398 42.2H12.9198V20.38H11.7198V43.4H31.9398L36.2998 39.04V17.2H39.1798V9.66003H39.1598ZM37.9598 16H10.0398V14.52L13.6798 10.88H37.9598V16ZM19.1796 22.14H20.3796V37.28H19.1796V22.14ZM27.6191 22.14H28.8191V37.28H27.6191V22.14Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+};
+
+export { DeleteLineLightIcon };
--- packages/icons/src/index.tsx
+++ packages/icons/src/index.tsx
@@ -1,12 +1,19 @@
-export { HomeLineIcon } from './icons/homeLine';
-export { HomeLineBoldIcon } from './icons/homeLineBold';
-export { HomeLineLightIcon } from './icons/homeLineLight';
-export { HomeFillIcon } from './icons/homeFill';
-export { UserLineIcon } from './icons/userLine';
-export { UserLineBoldIcon } from './icons/userLineBold';
-export { UserLineLightIcon } from './icons/userLineLight';
-export { UserFillIcon } from './icons/userFill';
+export { DeleteLineIcon } from './icons/deleteLine';
+export { DeleteLineBoldIcon } from './icons/deleteLineBold';
+export { DeleteLineLightIcon } from './icons/deleteLineLight';
+export { DeleteFillIcon } from './icons/deleteFill';
+
 export { EditLineIcon } from './icons/editLine';
 export { EditLineBoldIcon } from './icons/editLineBold';
 export { EditLineLightIcon } from './icons/editLineLight';
 export { EditFillIcon } from './icons/editFill';
+
+export { HomeLineIcon } from './icons/homeLine';
+export { HomeLineBoldIcon } from './icons/homeLineBold';
+export { HomeLineLightIcon } from './icons/homeLineLight';
+export { HomeFillIcon } from './icons/homeFill';
+
+export { UserLineIcon } from './icons/userLine';
+export { UserLineBoldIcon } from './icons/userLineBold';
+export { UserLineLightIcon } from './icons/userLineLight';
+export { UserFillIcon } from './icons/userFill';

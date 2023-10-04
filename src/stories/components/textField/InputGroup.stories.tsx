fc2070e0aaa4dc0afe9edb3d43c68536ebc22e0d
laura-maxst <jykim@maxst.com>
2023-10-04 10:30:55 +0900

    Squashed 'packages/maxst-design-system/' changes from cd62ebe..efd2de8
    efd2de8 Merged in develop (pull request #191)
    d34b01c Merged in feature/MDSYSTEM-159-npm-library-test (pull request #190)
    dc48c59 [MDSYSTEM-159] fullwidth의 input group 내 dropdown 사용시 스타일 수정 및 예시 추가
    
    git-subtree-dir: packages/maxst-design-system
    git-subtree-split: efd2de8ecdd2b1864f876e19f191eb5d69b415f8

--- src/stories/components/textField/InputGroup.stories.tsx
+++ src/stories/components/textField/InputGroup.stories.tsx
@@ -1,6 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
 import type { Meta, StoryObj } from '@storybook/react';
 import { InputGroup, TextField } from '@components/textField';
+import { Dropdown } from '@components/dropdown';
+
+import {
+  ArrowDownLineIcon,
+  CheckLineIcon,
+  ArrowRightLineIcon,
+} from '@maxst-designsystem/icons';
 
 const meta: Meta<typeof InputGroup> = {
   title: 'Components/InputGroup',
@@ -34,6 +41,55 @@ export const InputGroupDefault: Story = {
   },
 };
 
+const menuData = [
+  {
+    id: 'label-0',
+    label: 'label 0',
+    iconLeft: <CheckLineIcon />,
+    iconRight: <ArrowRightLineIcon />,
+    disabled: false,
+    subLabel: 'more',
+  },
+  {
+    id: 'label-1',
+    label: 'label 1',
+    iconLeft: <CheckLineIcon />,
+    iconRight: <ArrowRightLineIcon />,
+    disabled: true,
+    // subLabel: 'more',
+  },
+  {
+    id: 'label-2',
+    label: 'label 2',
+    iconRight: <ArrowRightLineIcon />,
+    disabled: false,
+    subLabel: 'more',
+  },
+  {
+    id: 'label-3',
+    label: 'label 3',
+    iconLeft: <CheckLineIcon />,
+    disabled: false,
+    subLabel: 'more',
+  },
+  {
+    id: 'label-4',
+    label: 'label 4',
+    iconLeft: <CheckLineIcon />,
+    iconRight: <ArrowRightLineIcon />,
+    disabled: false,
+    // subLabel: 'more',
+  },
+];
+
+const onClickMenu = (data: any) => {
+  console.log('click data', data);
+};
+
+const onChangeMenu = (data: any) => {
+  console.log('change data', data);
+};
+
 export const InputGroupFullWidth: Story = {
   args: {
     id: 'full-field',
@@ -41,7 +97,19 @@ export const InputGroupFullWidth: Story = {
     fullWidth: true,
     children: (
       <>
-        <TextField id="full-field01" placeholder="one" />
+        <Dropdown
+          menuData={menuData}
+          onChange={onChangeMenu}
+          onClick={onClickMenu}
+          id="phone-01"
+        >
+          <TextField
+            id=""
+            required={true}
+            placeholder="placeholder"
+            iconRight={<ArrowDownLineIcon />}
+          />
+        </Dropdown>
         <TextField id="full-field02" placeholder="two" />
         <TextField id="full-field03" placeholder="three" />
       </>

30dc6a950f7bd7b462db35774113779841648ffc
laura-maxst <jykim@maxst.com>
2023-12-01 14:48:13 +0900

    icon library build 결과물 및 버전 업데이트

---
+++ packages/icons/dist/icons/fileInPageFill.d.ts
@@ -0,0 +1,3 @@
+import React from 'react';
+declare const FileInPageFillIcon: () => React.JSX.Element;
+export { FileInPageFillIcon };
---
+++ packages/icons/dist/icons/fileInPageLineBold.d.ts
@@ -0,0 +1,3 @@
+import React from 'react';
+declare const FileInPageLineBoldIcon: () => React.JSX.Element;
+export { FileInPageLineBoldIcon };

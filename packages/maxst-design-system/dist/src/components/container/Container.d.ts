f7a4eeb845ab6c07c5d4544a2d443d9da6d8ce6f
laura-maxst <jykim@maxst.com>
2024-01-26 13:38:28 +0900

    ui library 1.0.9 update 및 빌드 결과물

--- packages/maxst-design-system/dist/src/components/container/Container.d.ts
+++ packages/maxst-design-system/dist/src/components/container/Container.d.ts
@@ -2,7 +2,9 @@ import React from 'react';
 interface ContainerProps {
     full?: boolean;
     className?: string;
+    maxWidth?: string;
+    align?: 'left' | 'center' | 'right';
     children: React.ReactNode;
 }
-declare const Container: ({ full, className, children, ...props }: ContainerProps) => JSX.Element;
+declare const Container: ({ full, className, maxWidth, align, children, ...props }: ContainerProps) => JSX.Element;
 export { Container };

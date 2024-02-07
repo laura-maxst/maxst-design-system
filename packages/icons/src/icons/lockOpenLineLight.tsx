9d137213cdc4d328d7f831b02c459db79814a294
laura-maxst <jykim@maxst.com>
2024-02-07 14:29:14 +0900

    icon library LockOpen Icon update

--- packages/icons/src/icons/lockOpenLineLight.tsx
+++ packages/icons/src/icons/lockOpenLineLight.tsx
@@ -3,17 +3,24 @@ import React from 'react';
 const LockOpenLineLightIcon = () => {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
       width="48"
       height="48"
       viewBox="0 0 48 48"
       fill="none"
+      xmlns="http://www.w3.org/2000/svg"
     >
-      <path
-        d="M32.6005 21.54V13.22C32.6005 8.48 28.7405 4.62 24.0005 4.62C19.2605 4.62 15.4005 8.48 15.4005 13.22V16H16.6005V13.22C16.6005 9.14 19.9205 5.82 24.0005 5.82C28.0805 5.82 31.4005 9.14 31.4005 13.22V21.54H14.9205L10.5605 25.9V43.42H33.0805L37.4405 39.06V21.54H32.6005ZM36.2405 38.56L32.6005 42.2H11.7805V26.38L15.4205 22.74H36.2405V38.56Z"
-        fill="currentColor"
-      />
-      <path d="M23.8211 32.02H22.6211V38H23.8211V32.02Z" fill="currentColor" />
+      <g clipPath="url(#clip0_2746_4538)">
+        <path
+          d="M32.6 13.22C32.6 8.48 28.74 4.62 24 4.62C19.26 4.62 15.4 8.48 15.4 13.22H16.6C16.6 9.14 19.92 5.82 24 5.82C28.08 5.82 31.4 9.14 31.4 13.22V21.4H14.98L10.66 25.78V43.4H32.98L37.34 39.04V21.4H32.6V13.22ZM36.14 38.56L32.5 42.2H11.86V26.28L15.48 22.6H36.14V38.56Z"
+          fill="currentColor"
+        />
+        <path d="M24.6 29.4H23.4V35.38H24.6V29.4Z" fill="currentColor" />
+      </g>
+      <defs>
+        <clipPath id="clip0_2746_4538">
+          <rect width="48" height="48" fill="white" />
+        </clipPath>
+      </defs>
     </svg>
   );
 };

6e099c864c725bebe7143c715d3761a6bd807cad
laura-maxst <jykim@maxst.com>
2024-02-08 11:13:05 +0900

    icon library MinusCircle Icon update

--- packages/icons/src/icons/minusCircleFill.tsx
+++ packages/icons/src/icons/minusCircleFill.tsx
@@ -9,12 +9,17 @@ const MinusCircleFillIcon = () => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M24 42C33.9411 42 42 33.9411 42 24C42 14.0589 33.9411 6 24 6C14.0589 6 6 14.0589 6 24C6 33.9411 14.0589 42 24 42ZM14 22.8V25.2H34V22.8H14Z"
-        fill="currentColor"
-      />
+      <g clipPath="url(#clip0_2754_24349)">
+        <path
+          d="M24 3.83997C12.92 3.83997 3.92 12.86 3.92 23.92C3.92 34.98 12.92 44 24 44C35.08 44 44.08 35 44.08 23.92C44.08 12.84 35.08 3.83997 24 3.83997ZM36.88 25.2H11.12V22.8H36.86V25.2H36.88Z"
+          fill="currentColor"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_2754_24349">
+          <rect width="48" height="48" fill="white" />
+        </clipPath>
+      </defs>
     </svg>
   );
 };

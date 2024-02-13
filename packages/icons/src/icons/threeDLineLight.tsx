6686dbfd8cffac685948ed9b1a9b4b5623463f51
laura-maxst <jykim@maxst.com>
2024-02-13 13:45:01 +0900

    icon library ThreeD Icon update

--- packages/icons/src/icons/threeDLineLight.tsx
+++ packages/icons/src/icons/threeDLineLight.tsx
@@ -3,40 +3,23 @@ import React from 'react';
 const ThreeDLineLightIcon = () => {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
       width="48"
       height="48"
       viewBox="0 0 48 48"
       fill="none"
+      xmlns="http://www.w3.org/2000/svg"
     >
-      <path
-        d="M17.8202 8.66003L8.66016 17.82V39.34H30.1802L39.3402 30.18V8.66003H17.8202ZM18.3202 9.86003H37.2802L29.6602 17.48H18.8002V16.54H17.6202V17.5H10.7002L18.3202 9.88003V9.86003ZM9.86016 18.68H29.3202V38.14H9.86016V18.68ZM30.5202 37.3V30.4H31.3002V29.2H30.5202V18.34L38.1402 10.72V29.68L30.5202 37.3Z"
-        fill="currentColor"
-      />
-      <path
-        d="M17.5674 20.7145L17.5527 21.9144L18.7526 21.9291L18.7673 20.7292L17.5674 20.7145Z"
-        fill="currentColor"
-      />
-      <path
-        d="M17.502 24.9278L17.4863 26.1277L18.6862 26.1434L18.7019 24.9435L17.502 24.9278Z"
-        fill="currentColor"
-      />
-      <path
-        d="M17.6661 12.3229L17.6504 13.5228L18.8503 13.5385L18.866 12.3386L17.6661 12.3229Z"
-        fill="currentColor"
-      />
-      <path
-        d="M12.1552 35.0099L11.3066 35.8584L12.1552 36.7069L13.0037 35.8584L12.1552 35.0099Z"
-        fill="currentColor"
-      />
-      <path
-        d="M15.121 32.0296L14.2725 32.8782L15.121 33.7267L15.9695 32.8782L15.121 32.0296Z"
-        fill="currentColor"
-      />
-      <path d="M18.7195 29.2H17.5195V30.4H18.7195V29.2Z" fill="currentColor" />
-      <path d="M27.1199 29.2H25.9199V30.4H27.1199V29.2Z" fill="currentColor" />
-      <path d="M35.5203 29.2H34.3203V30.4H35.5203V29.2Z" fill="currentColor" />
-      <path d="M22.9207 29.2H21.7207V30.4H22.9207V29.2Z" fill="currentColor" />
+      <g clipPath="url(#clip0_2758_36355)">
+        <path
+          d="M6.58 13.9401V34.0801L24 44.1401L41.42 34.0801V13.9401L24 3.86011L6.58 13.9401ZM39.64 14.2801L24 23.3001L8.36 14.2801L24 5.26011L39.64 14.2801ZM7.76 15.3201L23.4 24.3401V42.4001L7.76 33.3801V15.3201ZM24.6 42.4001V24.3401L40.24 15.3201V33.3801L24.6 42.4001Z"
+          fill="currentColor"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_2758_36355">
+          <rect width="48" height="48" fill="white" />
+        </clipPath>
+      </defs>
     </svg>
   );
 };

06c67ecd1e817eb749bd33ea81895bda30025a4a
laura-maxst <jykim@maxst.com>
2024-02-13 14:24:08 +0900

    icon library Recognition Icon update

--- packages/icons/src/icons/recognitionLineLight.tsx
+++ packages/icons/src/icons/recognitionLineLight.tsx
@@ -12,23 +12,23 @@ const RecognitionLineLightIcon = () => {
       <g clipPath="url(#clip0_2758_40585)">
         <path
           d="M7.79998 14.9001V7.8001H14.9V6.6001H6.59998V14.9001H7.79998Z"
-          fill="#222222"
+          fill="currentColor"
         />
         <path
           d="M40.2 33.1001V40.2001H33.1V41.4001H41.4V33.1001H40.2Z"
-          fill="#222222"
+          fill="currentColor"
         />
         <path
           d="M14.9 40.2001H7.79998V33.1001H6.59998V41.4001H14.9V40.2001Z"
-          fill="#222222"
+          fill="currentColor"
         />
         <path
           d="M33.1 7.8001H40.2V14.9001H41.4V6.6001H33.1V7.8001Z"
-          fill="#222222"
+          fill="currentColor"
         />
         <path
           d="M29.68 23.4001H24.6V18.3201H23.4V23.4001H18.32V24.6001H23.4V29.6801H24.6V24.6001H29.68V23.4001Z"
-          fill="#222222"
+          fill="currentColor"
         />
       </g>
       <defs>

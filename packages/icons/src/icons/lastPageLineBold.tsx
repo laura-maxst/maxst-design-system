0d27c1b217e953a28e66e0e8fcf3bfa9f652e9fe
laura-maxst <jykim@maxst.com>
2024-02-14 13:58:30 +0900

    icon library LastPage Icon update

--- packages/icons/src/icons/lastPageLineBold.tsx
+++ packages/icons/src/icons/lastPageLineBold.tsx
@@ -9,15 +9,15 @@ const LastPageLineBoldIcon = () => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clipPath="url(#clip0_2751_20610)">
+      <g clipPath="url(#clip0_999_141)">
         <path
-          d="M13.8 37.5L12.96 36.66L25.6 24.02L12.96 11.38L13.8 10.54L27.3 24.02L13.8 37.5Z"
+          d="M12.26 12.1L24.18 24.02L12.26 35.94L14.52 38.22L28.7 24.02L14.52 9.82001L12.26 12.1Z"
           fill="currentColor"
         />
-        <path d="M33.98 10.02H32.78V38.02H33.98V10.02Z" fill="currentColor" />
+        <path d="M34.98 10.02H31.78V38.02H34.98V10.02Z" fill="currentColor" />
       </g>
       <defs>
-        <clipPath id="clip0_2751_20610">
+        <clipPath id="clip0_999_141">
           <rect width="48" height="48" fill="white" />
         </clipPath>
       </defs>

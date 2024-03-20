921faabff1bcf0acd744f638cf1cde95d0b3657d
laura-maxst <jykim@maxst.com>
2024-03-20 10:57:00 +0900

    ui library v1.0.19 배포 및 빌드 결과물

--- packages/maxst-design-system/dist/src/components/footer/Footer.d.ts
+++ packages/maxst-design-system/dist/src/components/footer/Footer.d.ts
@@ -7,6 +7,8 @@ interface FooterProps {
     topRightArea?: React.ReactNode;
     bottomRightArea?: React.ReactNode;
     className?: string;
+    logo: any;
+    addressData?: string[];
 }
-declare const Footer: ({ termData, topRightArea, bottomRightArea, className, }: FooterProps) => JSX.Element;
+declare const Footer: ({ termData, topRightArea, bottomRightArea, className, logo, addressData, }: FooterProps) => JSX.Element;
 export { Footer };

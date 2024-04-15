5b2dca5269254fbcc5a8652e86bdea9d8aa7eb0d
laura-maxst <jykim@maxst.com>
2024-04-15 15:42:18 +0900

    Squashed 'packages/maxst-design-system/' changes from 40e14fe6..bfb0eeaf
    bfb0eeaf Merged in develop (pull request #428)
    9c418c04 Merged in feature/MDSYSTEM-271-loading-page (pull request #427)
    1073ec61 [MDSYSTEM-271] loading page component 개발 및 코드 정리
    18f5e997 [MDSYSTEM-271] loading inline block 속성 component 개발 및 lottie setting
    4fe2508e [MDSYSTEM-271] loading 기본 구조 개발
    
    git-subtree-dir: packages/maxst-design-system
    git-subtree-split: bfb0eeafb958a50832cea9ea8601d16d8916a71a

---
+++ src/stories/feedback/loading/Loading.stories.tsx
@@ -0,0 +1,57 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Loading } from '@components/loading';
+import { BoxGroup } from '@components/boxGroup';
+import { Title } from '@components/title';
+
+const meta: Meta<typeof Loading> = {
+  title: 'Feedback/Loading/Loading',
+  component: Loading,
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: 'Loading을 contents의 내부에서 inline의 요소로 사용합니다.',
+      },
+    },
+  },
+  argTypes: {
+    className: {
+      // description: 'Loading',
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Loading>;
+
+export const LoadingDefault: Story = {
+  args: {
+    size: 'l',
+    color: 'mint',
+  },
+};
+
+export const LoadingWithTitle: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Title이나 Text와 함께 inline 속성으로 사용될 경우 감싸고 있는 부모 div를 사용하여 정렬을 맞춰줍니다.',
+      },
+    },
+  },
+  render: (args) => (
+    <div className="loading-sample-wrap">
+      <BoxGroup>
+        <Title role={1} size={'m'}>
+          Title과 함께 쓰이는 Loading
+        </Title>
+        <Loading {...args} />
+      </BoxGroup>
+    </div>
+  ),
+  args: {
+    size: 's',
+    color: 'mint',
+  },
+};
---
+++ src/stories/feedback/loading/LoadingPage.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { LoadingPage } from '@components/loading';
+import LoadingSample from './LoadingSample';
+
+const meta: Meta<typeof LoadingPage> = {
+  title: 'Feedback/Loading/LoadingPage',
+  component: LoadingPage,
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: 'Loading 상태일 경우 호출하여 사용합니다.',
+      },
+    },
+  },
+  argTypes: {
+    className: {
+      description: 'Loading',
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof LoadingPage>;
+
+export const LoadingPageDefault: Story = {
+  render: (args) => (
+    <LoadingSample>
+      <LoadingPage {...args} />
+    </LoadingSample>
+  ),
+  args: {
+    size: 'l',
+  },
+};

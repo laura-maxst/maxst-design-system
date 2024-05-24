b82c4ff551dcd749daaa6039cbbafa208c49ad6b
laura-maxst <jykim@maxst.com>
2024-05-24 16:59:58 +0900

    ui library v1.0.39 배포 및 빌드 결과물

--- packages/maxst-design-system/dist/src/components/spinner/spinner.d.ts
+++ packages/maxst-design-system/dist/src/components/spinner/spinner.d.ts
@@ -1,7 +1,9 @@
 import React from 'react';
 interface spinnerPropsType {
+    width?: string;
+    height?: string;
     color?: 'white' | 'black';
     className?: string;
 }
-declare const Spinner: ({ color, className }: spinnerPropsType) => React.JSX.Element;
+declare const Spinner: ({ color, className, width, height }: spinnerPropsType) => React.JSX.Element;
 export { Spinner };
